1dc2db7fcc12d33e70cc34588a4d95d813415fec
Biamah <beatriz.souza.costa@outlook.com>
2025-07-06 20:33:14 -0300

    feat: cria movimento do circulo com base no movimento filtrado do mouse com delay

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,14 @@ let source = fromEvent(document, "mousemove").pipe(
   map((e: MouseEvent) => {
     return { x: e.clientX, y: e.clientY };
   }),
-  filter((value: mouseTrack) => value.x < 500)
+  filter((value: mouseTrack) => value.x < 500),
+  delay(200)
 );
 
 function onNext(value: mouseTrack) {
   console.log(value);
+  circle.style.left = `${value.x}px`;
+  circle.style.top = `${value.y}px`;
 }
 
 source.subscribe({
