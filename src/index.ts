9f1bf891395dba532f9eed867fa991f4e195dec5
Biamah <beatriz.souza.costa@outlook.com>
2025-07-06 14:37:33 -0300

    feat: cria primeira instancia do observable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-function component() {
-  const element = document.createElement("div");
+import { from, Observable, Subscriber } from "rxjs";
 
-  element.innerHTML = "teste";
+let numbers = [1, 2, 3, 5, 6];
 
-  return element;
-}
+let sourceInstance = new Observable((subscriber) => {
+  let index = 0;
+  let produceValue = () => {
+    for (let n of numbers) {
+      if (n > 5) subscriber.error("Aconteceu o erro esperado");
 
-document.body.appendChild(component());
+      subscriber.next(n);
+    }
+    subscriber.complete();
+  };
+  produceValue();
+});
+
+sourceInstance.subscribe({
+  next: (x: number) => console.log(x),
+  error: (e: Error) => console.log(e),
+  complete: () => console.log("Completo"),
+});
