5f14ad2fe1f8c8820fb20dcbec827785741ba277
Biamah <beatriz.souza.costa@outlook.com>
2025-07-06 14:46:36 -0300

    feat: deixa a instancia do observable assincrona e com timeout

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import { from, Observable, Subscriber } from "rxjs";
+import { Observable } from "rxjs";
 
 let numbers = [1, 2, 3, 5, 6];
 
 let sourceInstance = new Observable((subscriber) => {
   let index = 0;
   let produceValue = () => {
-    for (let n of numbers) {
-      if (n > 5) subscriber.error("Aconteceu o erro esperado");
-
-      subscriber.next(n);
+    subscriber.next(numbers[index++]);
+    // chamada recursiva a ele mesmo
+    if (index < numbers.length) {
+      setTimeout(produceValue, 1000);
+    } else {
+      subscriber.complete();
     }
-    subscriber.complete();
   };
   produceValue();
 });
