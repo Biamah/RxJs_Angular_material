6e72454e3cbd3c2f9ef0d9389e9256a7c9b99792
Biamah <beatriz.souza.costa@outlook.com>
2025-07-14 09:34:11 -0300

    feat: cria os primeiros componentes de testes

--- Angular-Material/src/app/app.ts
+++ Angular-Material/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSliderModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'Angular-Material';
