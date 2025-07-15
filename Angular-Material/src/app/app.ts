458f0f67ebe72cf99ef3b3e610beef62660eedff
Biamah <beatriz.souza.costa@outlook.com>
2025-07-15 00:12:51 -0300

    feat: adiciona comportamentos dinamicos ao sidenav usando observable

--- Angular-Material/src/app/app.ts
+++ Angular-Material/src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -18,5 +19,21 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'Angular-Material';
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngAfterContentInit(): void {
+    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
+      next: (res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      },
+    });
+  }
 }
